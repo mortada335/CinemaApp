14931268156abe5c33a62636fb80d94b019f04c5
mortada335 <a80923904@gmail.com>
2024-09-22 14:42:26 +0300

    Second Initial commit

--- vite.config.mjs
+++ vite.config.mjs
@@ -10,7 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://github.com/mortada335/CinemaApp.git',
+
   plugins: [
     Vue({
       template: { transformAssetUrls }
